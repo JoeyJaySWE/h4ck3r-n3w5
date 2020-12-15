4c476bfd3e85d28e266f84f028bc239bbe97ab4c
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2020-12-15 08:59:42 +0100

    updated views

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:site_name" content="Vengeful Scars">
+    <meta property="og:title" content="<?= $metaTitle; ?>" />
+    <meta property="og:description" content="<?= $metaDesc; ?>" />
+    <meta property="og:image" content="<?= $metaImg; ?>" />
+    <meta name="twitter:image" content="<?= $metaImg; ?>" />
+    <meta name="twitter:card" content="<?= $metaCard; ?>" />
+    <meta name="twitter:image:alt" content="<?= $metImgAlt; ?>">
+    <meta propety="og:url" content="http://vengefulscars.joeyjaydigital.com/img/" />
+    <title><?= $title; ?></title>
+</head>
+
+<body>
